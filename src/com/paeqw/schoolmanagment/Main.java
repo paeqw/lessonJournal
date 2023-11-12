ba7c0ae3e4c18e35c4cad2df786e4395deabd38f
paeqw <paeqw12@gmail.com>
2023-11-12 15:48:04 +0100

    added delete to menu

--- src/com/paeqw/schoolmanagment/Main.java
+++ src/com/paeqw/schoolmanagment/Main.java
@@ -1,5 +1,7 @@
 package com.paeqw.schoolmanagment;
 
+import java.util.Map;
+import java.util.*;
 import com.paeqw.collections.ClassCollection;
 import com.paeqw.collections.PersonCollection;
 import com.paeqw.collections.SubjectCollection;
@@ -8,10 +10,6 @@ import com.paeqw.models.*;
 import com.paeqw.models.Class;
 import com.paeqw.utils.InputHandler;
 import com.paeqw.utils.InputValidator;
-import java.util.Map;
-
-import java.util.*;
-
 import static com.paeqw.enums.DayOfWeek.*;
 
 public class Main {
@@ -29,6 +27,7 @@ public class Main {
                 System.out.println("2. Delete...");
                 System.out.println("3. Modify...");
                 System.out.println("4. Add...");
+                System.out.println("-1. Exit program.");
 
                 choice = inputHandler.getInt("Input your choice");
                 switch (choice) {
@@ -39,6 +38,7 @@ public class Main {
                         System.out.println("4. Show all students");
                         System.out.println("5. Show all classes");
                         System.out.println("6. Show lesson plan for a given class for a given day");
+                        System.out.println("7. Show all teacher's subjects");
                         int x = inputHandler.getInt("Your choice");
                         System.out.println();
 
@@ -87,9 +87,17 @@ public class Main {
                                     System.out.println(entry.getKey() + " - " + entry.getValue().getName());
                                 }
                             }
-                            default -> {
-                                System.out.println("wrong choice, enter number between 1 - 6");
+                            // 7. Show all teacher's subjects
+                            case 7 -> {
+                                String firstname = inputHandler.getString("Enter first name");
+                                String lastname = inputHandler.getString("Enter last name");
+
+                                int c = 0;
+                                for (var el : subjectCollection.searchAllTeachersSubjects(firstname, lastname)) {
+                                    System.out.println(++c + " " + el.getName());
+                                }
                             }
+                            default -> System.out.println("wrong choice, enter number between 1 - 7");
                         }
                         System.out.println();
                     }
@@ -102,11 +110,56 @@ public class Main {
                         System.out.println();
 
                         switch (x) {
+                            // 1. Delete a person
                             case 1 -> {
+                                System.out.println("\n" + "Deleting a person...");
+
                                 String firstname = inputHandler.getString("Enter first name");
                                 String lastname = inputHandler.getString("Enter last name");
 
-                                System.out.println("you are going to ");
+                                System.out.println("You are going to delete person: " + firstname+ " " + lastname);
+                                String y = inputHandler.getString("Are you sure? (y/n)");
+                                switch (y.toLowerCase()) {
+                                    case "y" -> {
+                                        personCollection.removePerson(firstname,lastname);
+                                        System.out.println("Succesfully deleted person " + firstname + " " + lastname);
+                                    }
+                                    case "n" -> System.out.println("Canceled.");
+                                    default -> System.err.println("Wrong choice.");
+                                }
+                            }
+                            // 2. Delete class
+                            case 2 -> {
+                                System.out.println("\n" + "Deleting a class...");
+
+                                String className = inputHandler.getString("Enter class name");
+                                System.out.println("You are going to delete class with name '"+ className + "'");
+                                String y = inputHandler.getString("Are you sure? (y/n)");
+                                switch (y.toLowerCase()) {
+                                    case "y" -> {
+                                        classCollection.removeClass(className);
+                                        System.out.println("Succesfully deleted class '" + className + "'");
+                                    }
+                                    case "n" -> System.out.println("Canceled.");
+                                    default -> System.err.println("Wrong choice.");
+                                }
+                            }
+                            // 3. Delete subject
+                            case 3 -> {
+                                System.out.println("\n" + "Deleting a subject...");
+
+                                String subjectName = inputHandler.getString("Enter subject name");
+
+                                System.out.println("You are going to delete subject with name '" + subjectName + "'");
+                                String y = inputHandler.getString("Are you sure? (y/n)");
+                                switch (y.toLowerCase()) {
+                                    case "y" -> {
+                                        subjectCollection.removeSubject(subjectName);
+                                        System.out.println("Succesfully deleted subject '" + subjectName + "'");
+                                    }
+                                    case "n" -> System.out.println("Canceled.");
+                                    default -> System.err.println("Wrong choice.");
+                                }
                             }
                         }
 
@@ -116,16 +169,20 @@ public class Main {
                         System.out.println("2. Modify class");
                         System.out.println("3. Modify subject");
                         System.out.println("4. Modify lesson plan for given class");
+
+                        int x = inputHandler.getInt("Your choice");
+                        System.out.println();
+
+                        switch (x) {
+
+                        }
+
                     }
                     case 4 -> {
                         System.out.println("pain.");
                     }
-                    case -1 -> {
-                        System.out.println("exiting program...");
-                    }
-                    default -> {
-                        System.out.println("wrong choice, enter number between 1 - x");
-                    }
+                    case -1 -> System.out.println("exiting program...");
+                    default -> System.out.println("wrong choice, enter number between 1 - x");
                 }
 
             } while(choice != -1);
